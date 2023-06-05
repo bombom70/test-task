97ae0bad30b33b552cd0e3f90c8f304be1d14dfc
bombom70 <33407752+bombom70@users.noreply.github.com>
2023-06-05 12:30:50 +0500

    Update accum.js

--- accum.js
+++ accum.js
@@ -1,10 +1,9 @@
 const accum = (str) => {
-	const chars = str.split('');
-	const lowerCasesChars = chars.map(c => c.toLowerCase());
-
-	return lowerCasesChars.reduce((acc, val, i) => {
-		const value = `${val.toUpperCase()}${val.repeat(i)}`;
-		acc.push(value);
-		return acc;
-	}, []).join('-');
+  const chars = str.split('');
+  const lowerCasesChars = chars.map(c => c.toLowerCase());
+  return lowerCasesChars.reduce((acc, val, i) => {
+    const value = `${val.toUpperCase()}${val.repeat(i)}`;
+    acc.push(value);
+    return acc;
+  }, []).join('-');
 }
