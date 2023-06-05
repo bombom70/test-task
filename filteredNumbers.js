69c518ccbe6ab4844c46a20b56e2dc0e2adf76bc
hamdamov-n <hamdamov-n@ugraweb.ru>
2023-06-05 12:28:20 +0500

    fix space

--- filteredNumbers.js
+++ filteredNumbers.js
@@ -1,13 +1,13 @@
 const filterNum = (data) => {
 	const numbers = data.filter(el => Number.isInteger(el));
 	numbers.sort((a, b) => {
-			if (a < b) {
-					return -1;
-				}
-				if (a > b) {
-					return 1;
-				}
-				return 0;
+		if (a < b) {
+			return -1;
+		}
+		if (a > b) {
+			return 1;
+		}
+		return 0;
 	});
 	return numbers;
 }
